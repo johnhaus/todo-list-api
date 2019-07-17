4744534688684db88814f6fb4953a002e15ed81c
johnhaus <john.haus@outlook.com>
2019-07-17 15:23:07 -0300

    set up a dummy v2 controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # namespace the controllers without affecting the URI
+  scope module: :v2, constraints: ApiVersion.new('v2') do
+    resources :todos, only: :index
+  end
+
+  # non-default versions (as in v2 above) have to be defined above
+  # the default version (v1 below in this case).
   scope module: :v1, constraints: ApiVersion.new('v1', true) do
     resources :todos do
       resources :items
